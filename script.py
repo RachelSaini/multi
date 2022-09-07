699dfcfb716ff2a86badfe487ec616dc9e60975a
ks40698 <110074646+ks40698@users.noreply.github.com>
2022-09-07 16:18:31 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,44 @@
+HELP_MESSAGE = [""""
+**Supported upload hosts:**
+'+----+-------------+---------+
+|    |     Host    | MaxSize |
++====+=============+=========+
+|  1 |  anonfiles  |  20 GB  |
++----+-------------+---------+
+|  2 |    Catbox   |  200 MB |
++----+-------------+---------+
+|  3 |   file.io   |   2 GB  |
++----+-------------+---------+
+|  4 |   Filemail  |   5 GB  |
++----+-------------+---------+
+|  5 |    Gofile   |  unlim  |
++----+-------------+---------+
+|  6 |    MegaUp   |   5 GB  |
++----+-------------+---------+
+|  7 |   MixDrop   |  unlim  |
++----+-------------+---------+
+|  8 |  pixeldrain |  10 GB  |
++----+-------------+---------+
+|  9 |    Racaty   |  unlim  |
++----+-------------+---------+
+| 10 | transfer.sh |  unlim  |
++----+-------------+---------+
+| 11 |     Uguu    |  128 MB |
++----+-------------+---------+
+| 12 |  WeTransfer |   2 GB  |
++----+-------------+---------+
+| 13 |  workupload |   2 GB  |
++----+-------------+---------+
+| 14 |  zippyshare |  500 MB |
++----+-------------+---------+'
+
+**Supported links: G-Drive url, TG file, DDL**
+
+ex: Gdrive to anonfiles:
+`/up gdrive-url #1`
+
+ex: TG file to WeTransfer:
+reply to a file with `/up #12`
+
+**Made by [@HxBots](https://t.me/hxBots) 🧪**
+""""]
