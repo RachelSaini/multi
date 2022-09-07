ccb5661541cd6e30bc881c837cb7dd6c585429a2
ks40698 <110074646+ks40698@users.noreply.github.com>
2022-09-07 16:25:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,6 @@ authorized_list = json.loads(os.getenv('authorized_list'))
 app = Client("my_account", api_id=os.getenv('api_id'),
              api_hash=os.getenv('api_hash'), bot_token=os.getenv('bot_token')
 
-if not os.path.exists('Downloads'):
-os.makedirs('Downloads')
 
 print("Bot started by @oVo-HxBots", flush=True)
 @app.on_message(filters.text)
